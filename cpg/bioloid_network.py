8f1f803fef46ec47b6be8f01f3668cda4b5b170f
horken7 <horken7@me.com>
2017-10-10 16:54:37 +0200

    trying to solve stuff, not sure if I succeed.

--- cpg/bioloid_network.py
+++ cpg/bioloid_network.py
@@ -12,6 +12,7 @@ class BioloidNetwork:
         self.size = len(weights) # how many masuoka neurons there are in the network
         self.weights = weights
         self.outputs = np.random.rand(self.size) # init the outputs randomly to start the network, then store the last ouput here
+        # self.outputs = np.ones(self.size) # init outputs with ones to avoid stocastic behaviour
         self.neurons = [] # where we store the matsuoka neurons
 
         self.stored_outputs = np.zeros([self.simulation_time, self.size]) # storing the output over time
@@ -25,18 +26,36 @@ class BioloidNetwork:
 
     def simulate_neurons(self):
         """ Simulate the neurons for 'simulation_time' amount of updates, NOTE: done sequentially, not in parallel. """
+        # self.outputs = np.ones(self.size)  # init outputs with ones to avoid stocastic behaviour
         for timestep in range(self.simulation_time):
-            for index, neuron in enumerate(self.neurons):
+            tmp_outputs = np.ones(self.size) # use this to make syncronous update
+            for index, neuron in enumerate(self.neurons): # TODO remake with matrices
                 input1 = sum(self.weights[index] * self.outputs) # inputs given based on some definition.. may change (TODO, which definition?)
                 input2 = 0
                 output = neuron.get_output(input1=input1, input2=input2, timestep=0.01) # REMEMBER, use timestep 0.01, not floating timestep!! compare constant to accelerometer timestep
-                self.outputs[index] = output
+                tmp_outputs[index] = output
                 self.stored_outputs[timestep][index] = output
+            self.outputs = tmp_outputs
 
     def get_outputs(self):
         """ Simple getter """
         return self.stored_outputs
 
+    def get_neurons(self):
+        """ Simple getter """
+        return self.neurons
+
+    def get_output(self):
+        """ Simple getter """
+        return self.outputs
+
     def set_weights(self, w):
         """ Simple setter """
         self.weights = w
+
+    def set_neurons(self, n):
+        """ Simple setter """
+        self.neurons = n
+
+    def set_output(self, o):
+        self.outputs = o
